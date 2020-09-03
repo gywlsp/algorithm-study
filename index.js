0d3f764ca5c9670f188d89426afc872f81459922
박효진 <gywlsp@MacBook-Pro.local>
2020-09-03 17:38:13 +0900

    feat: :sparkles: add javascript console default input form

---
+++ index.js
@@ -0,0 +1,17 @@
+//기본 양식
+
+let input = [];
+
+require("readline")
+  .createInterface(process.stdin, process.stdout)
+  .on("line", function (line) {
+    input.push(line.trim());
+    /*
+    1
+    2 3
+    4 5 6
+    콘솔창을 통해 위와 같은 입력이 들어왔을 때,
+    input에 ['1', '2 3', '4 5 6']이 저장된다.
+    */
+  })
+  .on("close", function () {});
