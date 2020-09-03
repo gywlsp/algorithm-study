d9714067678bc08ca90bd372ce08bab46286a3cc
박효진 <gywlsp@MacBook-Pro.local>
2020-09-03 18:11:27 +0900

    feat: :sparkles: add codeup q1007 source

---
+++ codeup/1007.js
@@ -0,0 +1,12 @@
+//https://codeup.kr/problem.php?id=1007
+
+let input = [];
+
+require("readline")
+  .createInterface(process.stdin, process.stdout)
+  .on("line", function (line) {
+    input.push(line.trim());
+  })
+  .on("close", function () {
+    console.log("C:\\Download\\hello.js");
+  });
