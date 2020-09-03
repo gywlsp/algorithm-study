61b31216af9f545c4f48ce9543f14e9f0d21f5d5
박효진 <gywlsp@MacBook-Pro.local>
2020-09-03 18:34:37 +0900

    feat: :sparkles: add codeup 1024 source

---
+++ codeup/1024.js
@@ -0,0 +1,12 @@
+//https://codeup.kr/problem.php?id=1024
+
+let input = [];
+
+require("readline")
+  .createInterface(process.stdin, process.stdout)
+  .on("line", function (line) {
+    input.push(line.trim());
+  })
+  .on("close", function () {
+    console.log(input[0].split("").join("\n"));
+  });
