5499a801680d419eb6586cb0c36500fa7d7ba5ec
박효진 <gywlsp@MacBook-Pro.local>
2020-09-05 01:10:25 +0900

    feat: :sparkles: add codeup q1082 source

---
+++ codeup/1082.js
@@ -0,0 +1,19 @@
+//https://codeup.kr/problem.php?id=1082
+
+let input = [];
+
+require("readline")
+  .createInterface(process.stdin, process.stdout)
+  .on("line", function (line) {
+    input.push(line.trim());
+  })
+  .on("close", function () {
+    const inputNum = parseInt(input[0], 16);
+    [...Array(15)]
+      .map((_, i) => i + 1)
+      .forEach((n) =>
+        console.log(
+          `${input[0]}*${n.toString(16)}=${(inputNum * n).toString(16)}`
+        )
+      );
+  });
