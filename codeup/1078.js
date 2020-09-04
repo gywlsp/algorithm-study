dfeab5d5a2591182a4e00dbef71b95927e2e7cdb
박효진 <gywlsp@MacBook-Pro.local>
2020-09-05 00:02:16 +0900

    feat: :sparkles: add codeup q1078 source

---
+++ codeup/1078.js
@@ -0,0 +1,16 @@
+//https://codeup.kr/problem.php?id=1078
+
+let input = [];
+
+require("readline")
+  .createInterface(process.stdin, process.stdout)
+  .on("line", function (line) {
+    input.push(line.trim());
+  })
+  .on("close", function () {
+    console.log(
+      [...Array(Number(input[0]))]
+        .map((_, i) => i + 1)
+        .reduce((acc, curr) => acc + (curr % 2 === 0 ? curr : 0), 0)
+    );
+  });
