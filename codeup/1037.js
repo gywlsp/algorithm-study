616170d7d3cb822814d4be7b07e7e73a937ed94c
박효진 <gywlsp@MacBook-Pro.local>
2020-09-04 22:46:15 +0900

    feat: :sparkles: add codeup q1037 source

---
+++ codeup/1037.js
@@ -0,0 +1,12 @@
+//https://codeup.kr/problem.php?id=1037
+
+let input = [];
+
+require("readline")
+  .createInterface(process.stdin, process.stdout)
+  .on("line", function (line) {
+    input.push(line.trim());
+  })
+  .on("close", function () {
+    console.log(String.fromCharCode(Number(input[0])));
+  });
