fe87ce4b84fa67d45872fe3b0ba5d93a0fbd375e
박효진 <gywlsp@MacBook-Pro.local>
2020-09-04 23:49:17 +0900

    feat: :sparkles: add codeup q1076 source

---
+++ codeup/1076.js
@@ -0,0 +1,17 @@
+//https://codeup.kr/problem.php?id=1076
+
+let input = [];
+
+require("readline")
+  .createInterface(process.stdin, process.stdout)
+  .on("line", function (line) {
+    input.push(line.trim());
+  })
+  .on("close", function () {
+    const inputAsciiCode = input[0].charCodeAt(0);
+    const n = inputAsciiCode - "a".charCodeAt(0);
+    [...Array(n)].forEach((_, i) =>
+      console.log(String.fromCharCode(inputAsciiCode - (n - i)))
+    );
+    console.log(input[0]);
+  });
