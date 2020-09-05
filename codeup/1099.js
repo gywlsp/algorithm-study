453dff8602752c62b9aa38cf9c769827c434d7a9
gywlsp <gywls517@gmail.com>
2020-09-05 21:58:16 +0900

    feat: :sparkles: add codeup q1099 source

---
+++ codeup/1099.js
@@ -0,0 +1,41 @@
+//https://codeup.kr/problem.php?id=1099
+
+let input = [];
+
+const strToNumArr = (str) => str.split(" ").map((numChar) => Number(numChar));
+
+require("readline")
+  .createInterface(process.stdin, process.stdout)
+  .on("line", function (line) {
+    input.push(line.trim());
+  })
+  .on("close", function () {
+    const result = input.map((str) => strToNumArr(str));
+    let x = 1,
+      y = 1;
+    while (true) {
+      result[y][x] = 9;
+      if (result[y][x + 1] === 2) {
+        result[y][x + 1] = 9;
+        break;
+      }
+      if (result[y][x + 1] === 0) {
+        x++;
+        continue;
+      }
+      if (result[y][x + 1] === 1) {
+        if (result[y + 1][x] === 2) {
+          result[y + 1][x] = 9;
+          break;
+        }
+        if (result[y + 1][x] === 0) {
+          y++;
+          continue;
+        }
+        if (result[y + 1][x] === 1) {
+          break;
+        }
+      }
+    }
+    result.forEach((arr) => console.log(arr.join(" ")));
+  });
