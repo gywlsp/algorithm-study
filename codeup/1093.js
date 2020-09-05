b173b38a096e30c64aaf946546226a21475c56fe
박효진 <gywlsp@MacBook-Pro.local>
2020-09-05 13:34:51 +0900

    feat: :sparkles: add codeup q1003 source

---
+++ codeup/1093.js
@@ -0,0 +1,16 @@
+//https://codeup.kr/problem.php?id=1093
+
+let input = [];
+
+const strToNumArr = (str) => str.split(" ").map((numChar) => Number(numChar));
+
+require("readline")
+  .createInterface(process.stdin, process.stdout)
+  .on("line", function (line) {
+    input.push(line.trim());
+  })
+  .on("close", function () {
+    const countArr = [...Array(23)].map(() => 0);
+    strToNumArr(input[1]).forEach((num) => (countArr[num - 1] += 1));
+    countArr.forEach((count) => console.log(count));
+  });
