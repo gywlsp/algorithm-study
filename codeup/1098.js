591f884cce152f9067f16465e2534a2d126b8012
박효진 <gywlsp@MacBook-Pro.local>
2020-09-05 14:23:38 +0900

    feat: :sparkles: add codeup q1098 source

---
+++ codeup/1098.js
@@ -0,0 +1,27 @@
+//https://codeup.kr/problem.php?id=1098
+
+let input = [];
+
+const strToNumArr = (str) => str.split(" ").map((numChar) => Number(numChar));
+
+require("readline")
+  .createInterface(process.stdin, process.stdout)
+  .on("line", function (line) {
+    input.push(line.trim());
+  })
+  .on("close", function () {
+    let inputIndex = 0;
+    const [h, w] = strToNumArr(input[inputIndex++]);
+    const result = [...Array(h)].map(() => [...Array(w)].map(() => 0));
+    let t = Number(input[inputIndex++]);
+    while (t--) {
+      let [l, d, x, y] = strToNumArr(input[inputIndex++]);
+      x--;
+      y--;
+      while (l--) {
+        if (d === 0) result[x][y++] = 1;
+        if (d === 1) result[x++][y] = 1;
+      }
+    }
+    result.forEach((arr) => console.log(arr.join(" ")));
+  });
