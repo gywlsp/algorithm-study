b7129f8c6fdb8c8cd54dbd0e1c2cda96f9b0d55c
gywlsp <gywls517@gmail.com>
2020-09-15 09:55:57 +0900

    feat: :sparkles: add baekjoon q6603 source

---
+++ baekjoon/6603.js
@@ -0,0 +1,36 @@
+//https://www.acmicpc.net/problem/6603
+
+let input = [];
+
+const strToNumArr = (str) => str.split(" ").map((numChar) => Number(numChar));
+
+const getCombinations = function (arr, selectNum) {
+  const results = [];
+  if (selectNum === 1) return arr.map((value) => [value]);
+
+  arr.forEach((fixed, index) => {
+    const rest = arr.slice(index + 1);
+    const combinations = getCombinations(rest, selectNum - 1);
+    const attached = combinations.map((combination) => [fixed, ...combination]);
+    results.push(...attached);
+  });
+
+  return results;
+};
+
+require("readline")
+  .createInterface(process.stdin, process.stdout)
+  .on("line", function (line) {
+    input.push(line.trim());
+  })
+  .on("close", function () {
+    let t = input.length - 1;
+    let inputIndex = 0;
+    while (t--) {
+      const [n, ...numList] = strToNumArr(input[inputIndex++]);
+      getCombinations(numList, 6).forEach((numList) =>
+        console.log(numList.join(" "))
+      );
+      console.log("");
+    }
+  });
