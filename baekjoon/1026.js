b071d5544c11f64f2b429165aea0f61037364d5c
gywlsp <gywls517@gmail.com>
2020-09-27 15:00:59 +0900

    feat: :sparkles: add baekjoon q1026 source

---
+++ baekjoon/1026.js
@@ -0,0 +1,19 @@
+//https://www.acmicpc.net/problem/1026
+
+let input = [];
+
+const strToNumArr = (str) => str.split(" ").map((numChar) => Number(numChar));
+
+require("readline")
+  .createInterface(process.stdin, process.stdout)
+  .on("line", function (line) {
+    input.push(line.trim());
+  })
+  .on("close", function () {
+    const A = strToNumArr(input[1]).sort((a, b) => a - b);
+    const B = strToNumArr(input[2]).sort((a, b) => b - a);
+    const result = A.reduce((acc, curr, index)=>{
+        return acc + curr * B[index]
+    }, 0);
+    console.log(result);
+  });
