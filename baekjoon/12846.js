9ab5776b70d52798a9c75b00f3405f92419cee75
gywlsp <gywls517@gmail.com>
2020-11-07 21:34:03 +0900

    refactor: :recycle: refactor baekjoon q12846 source code

--- baekjoon/12846.js
+++ baekjoon/12846.js
@@ -1,10 +1,7 @@
 //https://www.acmicpc.net/problem/12846
 
 let input = [];
-const stack = [];
 let result = 0;
-let temp, temp_start_index;
-let stack_top_index, stack_top_pay;
 
 const strToNumArr = (str) => str.split(" ").map((numString) => Number(numString));
 
@@ -16,35 +13,19 @@ require("readline")
   .on("close", function () {
     const N = Number(input[0]);
     const DAILY_PAY_LIST = strToNumArr(input[1]);
-    for(let i=0; i<N; i++){
-      const today_pay = DAILY_PAY_LIST[i];
-      if(stack.length===0){
-        stack.push([i, today_pay]);
-        continue;
-      }
 
-      [stack_top_index, stack_top_pay] = stack[stack.length-1];
-      if(stack_top_pay < today_pay){
-        stack.push([i, today_pay]);
-        continue;
+    let todayPay, day, dailyPay;
+    DAILY_PAY_LIST.push(0);
+    const stack = [[0, DAILY_PAY_LIST[0]]];
+
+    for(let i=1; i<=N; i++){
+      todayPay = DAILY_PAY_LIST[i];
+      while(stack.length!==0 && stack[stack.length-1][1]>todayPay){
+        [day, dailyPay] = stack.pop();
+        result = Math.max(result, dailyPay*(i-day));
       }
-      if(stack_top_pay > today_pay){
-        while(stack.length !== 0 && stack_top_pay > today_pay){
-          [stack_top_index, stack_top_pay] = stack[stack.length-1];
-          temp = stack_top_pay * (i-stack_top_index);
-          temp_start_index = stack_top_index;
-          if(temp > result) result = temp;
-          stack.pop();
-        }
-        stack.push([temp_start_index, today_pay]);
-      }
+      stack.push([i, todayPay]);
     }
-    
-    while(stack.length!==0){
-      [stack_top_index, stack_top_pay] = stack[stack.length-1];
-      temp = stack_top_pay * (N-stack_top_index);
-      if(temp>result) result=temp;
-      stack.pop();
-    }
+
     console.log(result);
   });
