512f5ae68fcbf42587dc26eb741ab3cdfd4f1931
gywlsp <gywls517@gmail.com>
2020-11-11 11:52:06 +0900

    feat: :sparkles: add baekjoon q10830 source

--- baekjoon/10830.js
+++ baekjoon/10830.js
@@ -3,8 +3,34 @@
 let input = [];
 const strToNumArr = (str) => str.split(" ").map((numString) => Number(numString));
 
-let result;
-const inputMatrix = [];
+let n;
+
+const multiplyMatrix = (_matrixA, _matrixB) => {
+  const result = [...Array(n)].map(() => [...Array(n)].map(() => 0));
+  
+  for(let i=0; i<n; i++){
+    for(let j=0; j<n; j++){
+      for(let k=0; k<n; k++){
+        result[i][j] += _matrixA[i][k] * _matrixB[k][j];
+      }
+      result[i][j] = result[i][j] % 1000;
+    }
+  }
+  return result;
+};
+
+const solve = (_matrix, _power) => {
+  if(_power === 1) return _matrix;
+
+  const dividedMatrix = solve(_matrix, Math.floor(_power/2));
+  const multipliedMatrix = multiplyMatrix(dividedMatrix, dividedMatrix);
+  return _power%2 === 1 ? multiplyMatrix(multipliedMatrix, _matrix) : multipliedMatrix;
+};
+
+const getInputMatrix = () => {
+  input.splice(0,1);
+  return input.map((row) => row.split(" ").map((numString) => Number(numString)%1000));
+};
 
 require("readline")
   .createInterface(process.stdin, process.stdout)
@@ -13,17 +39,9 @@ require("readline")
   })
   .on("close", function () {
     const [N, B] = strToNumArr(input[0]);
-    // for(let i=1; i<=N; i++){
-    //     inputMatrix.push(strToNumArr(input[i]));
-    // }
+    n = N;
+    const inputMatrix = getInputMatrix();
 
-    let temp = B;
-    let count = 0;
-
-    while(true){
-        temp = Math.floor(temp/2);
-        if(temp>0) count++;
-        else break;
-    }
-    console.log(count);
+    const result = solve(inputMatrix, B);
+    result.forEach((arr) => console.log(arr.join(" ")));
   });
