8d2bdd9e9293bf30f038cade7ec57c6189993a1d
gywlsp <gywls517@gmail.com>
2020-11-18 10:27:14 +0900

    feat: :sparkles: add baekjoon q1158 source

---
+++ baekjoon/1158.js
@@ -0,0 +1,27 @@
+//https://www.acmicpc.net/problem/1158
+
+const input = [];
+const result = [];
+
+const strToNumArr = (str) => str.split(" ").map((numString) => Number(numString));
+
+require("readline")
+  .createInterface(process.stdin, process.stdout)
+  .on("line", function (line) {
+    input.push(line.trim());
+  })
+  .on("close", function () {
+    const [N, K] = strToNumArr(input[0]);
+    
+    const numList = [...Array(N)].map((_, i)=>i+1);
+    let indexArrow = 0;
+    const interval = K-1;
+
+    while(numList.length!==0){
+        indexArrow = (indexArrow+interval)%numList.length;
+        result.push(numList[indexArrow]);
+        numList.splice(indexArrow, 1);
+    }
+
+    console.log(`<${result.join(", ")}>`);
+  });
