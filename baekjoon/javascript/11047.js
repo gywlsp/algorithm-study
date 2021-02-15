e74cefaec0913e4ccde8d93ca8d4491aecc51bd1
gywlsp <gywls517@gmail.com>
2021-02-15 21:35:54 +0900

    feat: :sparkles: add baekjoon q11047 javascript source

---
+++ baekjoon/javascript/11047.js
@@ -0,0 +1,32 @@
+const input = [];
+const strToNumArr = (str) =>
+  str.split(" ").map((numString) => Number(numString));
+
+require("readline")
+  .createInterface(process.stdin, process.stdout)
+  .on("line", function (line) {
+    input.push(line.trim());
+  })
+  .on("close", function () {
+    const [N, K] = strToNumArr(input.shift());
+    const values = input.map((v) => Number(v));
+
+    let k = K,
+      v,
+      q,
+      result = 0;
+    for (let i = N - 1; i >= 0; i--) {
+      v = values[i];
+      q = Math.floor(k / v);
+      if (q === 0) {
+        continue;
+      }
+      result += q;
+      k %= v;
+      if (k === 0) {
+        break;
+      }
+    }
+
+    console.log(result);
+  });
