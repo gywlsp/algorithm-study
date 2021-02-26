6613b5283d594ce004aed72aa423b2626cf1c7b0
gywlsp <gywls517@gmail.com>
2021-02-26 13:15:27 +0900

    feat: :sparkles: add baekjoon q2805 javascript source

--- baekjoon/javascript/2805.js
+++ baekjoon/javascript/2805.js
@@ -1,6 +1,10 @@
 //https://www.acmicpc.net/problem/2805
 
 const input = [];
+let n,
+  m,
+  result = 0;
+let tree_heights;
 
 const strToNumArr = (str) => str.split(" ").map(Number);
 
@@ -10,21 +14,32 @@ require("readline")
     input.push(line.trim());
   })
   .on("close", function () {
-    const [N, M] = strToNumArr(input[0]);
-    const TREE_HEIGHTS = strToNumArr(input[1]).sort((a, b) => b - a);
-    let max = TREE_HEIGHTS[0];
-    let min = max - M > 0? max - M : 0;
-    while (true) {
-      if (max - 1 === min || N === 1) break;
-      let temp = Math.floor((max + min) / 2);
-      let i = 0;
-      let sum = 0;
-      do {
-        sum += TREE_HEIGHTS[i] - temp;
-        i++;
-      } while (i < N && sum < M && TREE_HEIGHTS[i] > temp);
-      if(sum < M) max = temp;
-      else min = temp;
-      }
-    console.log(min);
+    [n, m] = strToNumArr(input[0]);
+    tree_heights = strToNumArr(input[1]).sort((a, b) => b - a);
+    const max = tree_heights[0];
+    const min = max - m > 0 ? max - m : 0;
+
+    binarySearch(min, max);
+    console.log(result);
   });
+
+const binarySearch = (min, max) => {
+  if (max < min) {
+    return;
+  }
+
+  const mid = Math.floor((max + min) / 2);
+  let sum = 0;
+  for (let i = 0; i < n; i++) {
+    const remain = tree_heights[i] - mid;
+    if (remain <= 0 || sum >= m) {
+      break;
+    }
+    sum += remain;
+  }
+  if (sum >= m) {
+    result = Math.max(mid, result);
+    return binarySearch(mid + 1, max);
+  }
+  return binarySearch(min, mid - 1);
+};
