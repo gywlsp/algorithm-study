4b0229cf3f9630db3451954cf2212d03172ad769
gywlsp <gywls517@gmail.com>
2021-02-27 09:06:55 +0900

    feat: :sparkles: add baekjoon q2178 javascript source

---
+++ baekjoon/javascript/2178.js
@@ -0,0 +1,42 @@
+const input = [];
+const dr = [1, -1, 0, 0];
+const dc = [0, 0, 1, -1];
+
+require("readline")
+  .createInterface(process.stdin, process.stdout)
+  .on("line", function (line) {
+    input.push(line.trim());
+  })
+  .on("close", function () {
+    const [N, M] = input.shift().split(" ").map(Number);
+    const maze = input.map((str) => str.split("").map(Number));
+    const mark = input.map(() => Array(M).fill(0));
+
+    const queue = [[0, 0]];
+    mark[0][0] = 1;
+    let r, c;
+    while (queue.length !== 0) {
+      [r, c, depth] = queue.shift();
+
+      for (let i = 0; i < 4; i++) {
+        let nextR = r + dr[i],
+          nextC = c + dc[i];
+        if (
+          nextR < 0 ||
+          nextR >= N ||
+          nextC < 0 ||
+          nextC >= M ||
+          !maze[nextR][nextC] ||
+          mark[nextR][nextC]
+        ) {
+          continue;
+        }
+        mark[nextR][nextC] = mark[r][c] + 1;
+        if (nextR === N - 1 && nextC === M - 1) {
+          console.log(mark[N - 1][M - 1]);
+          process.exit(0);
+        }
+        queue.push([nextR, nextC]);
+      }
+    }
+  });
