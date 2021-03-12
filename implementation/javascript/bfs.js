aa657f3b043e85e7884539ee80f9fae55a76f3c1
gywlsp <gywls517@gmail.com>
2021-03-12 20:06:53 +0900

    feat: :sparkles: add javascript bfs

---
+++ implementation/javascript/bfs.js
@@ -0,0 +1,33 @@
+const graph = [
+    [],
+    [2, 3, 8],
+    [1, 7],
+    [1, 4, 5],
+    [3, 5],
+    [3, 4],
+    [7],
+    [2, 6, 8],
+    [1, 7]
+];
+const visited = Array(graph.length).fill(false);
+const result = [];
+
+//큐
+const bfs = (graph, start, visited) => {
+    const queue = [start];
+    while(queue.length){
+        const v = queue.shift();
+        visited[v] = true;
+        result.push(v);
+        graph[v].forEach((u)=>{
+            if(!visited[u]){
+                visited[u] = true;
+                queue.push(u);
+            }
+        })
+    }
+}
+
+bfs(graph, 1, visited);
+console.log(result.join(" "));
+
