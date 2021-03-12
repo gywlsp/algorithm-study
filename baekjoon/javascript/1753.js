c9e5f8e0eab53cac232b52f8552a10d4953ebcd9
gywlsp <gywls517@gmail.com>
2021-03-12 20:28:11 +0900

    refactor: :recycle: refactor baekjoon q1753 javascript source

--- baekjoon/javascript/1753.js
+++ baekjoon/javascript/1753.js
@@ -1,7 +1,8 @@
 //https://www.acmicpc.net/problem/1753
 
-const input = [];
-let graph, visited, distances;
+const input = [],
+  graph = [];
+let visited, distances;
 const INF = 987654321;
 
 const strToNumArr = (str) => str.split(" ").map(Number);
@@ -12,54 +13,56 @@ require("readline")
     input.push(line.trim());
   })
   .on("close", function () {
-      const [V, E] = strToNumArr(input.shift());
-      const START_V = Number(input.shift());
+    const [V, _] = strToNumArr(input.shift());
+    const start = Number(input.shift());
 
-      graph=[...Array(V)].map(()=>[]);
-      visited=Array(V).fill(false);
-      distances=Array(V).fill(INF);
-      
-      let u, v, w;
-      input.forEach((str)=>{
-        [u, v, w] = strToNumArr(str);
-        graph[u-1].push([w, v-1]);
-      });
+    for (let i = 0; i < V; i++) {
+      graph.push([]);
+    }
+    visited = Array(V).fill(false);
+    distances = Array(V).fill(INF);
 
-      dijkstra(START_V-1);
-      
-      distances.forEach((d, i)=>{
-          if(d===INF){
-              distances[i]="INF";
-          }
-      })
-      console.log(distances.join("\n"));
+    let u, v, w;
+    input.forEach((str) => {
+      [u, v, w] = strToNumArr(str);
+      graph[u - 1].push([w, v - 1]);
+    });
+
+    dijkstra(start - 1);
+
+    distances.forEach((d, i) => {
+      if (d === INF) {
+        distances[i] = "INF";
+      }
+    });
+    console.log(distances.join("\n"));
   });
 
-
 const dijkstra = (vStart) => {
-    distances[vStart]=0;
+  distances[vStart] = 0;
 
-    while(true){
-        let closestW=INF, closestV;
-        for(let i=0; i<distances.length; ++i){
-            if(distances[i]<closestW && !visited[i]){
-                closestW=distances[i];
-                closestV=i;
-            }
-        }
-
-        if(closestW===INF){
-            break;
-        }
-
-        visited[closestV]=true;
-        let w, v;
-        graph[closestV].forEach((node)=>{
-            [w, v] = node;
-            if(visited[v]){
-                return;
-            }
-            distances[v]=Math.min(distances[v], distances[closestV]+w);
-        })
+  while (true) {
+    let shortestD = INF,
+      closestV;
+    for (let i = 0; i < distances.length; ++i) {
+      if (distances[i] < shortestD && !visited[i]) {
+        shortestD = distances[i];
+        closestV = i;
+      }
     }
+
+    if (shortestD === INF) {
+      break;
+    }
+
+    visited[closestV] = true;
+    let w, v;
+    graph[closestV].forEach((node) => {
+      [w, v] = node;
+      if (visited[v]) {
+        return;
+      }
+      distances[v] = Math.min(distances[v], distances[closestV] + w);
+    });
+  }
 };
