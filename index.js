acbe16bd82c7798a4adbc0780200a3666619fa34
gywlsp <gywls517@gmail.com>
2021-03-13 13:19:48 +0900

    refactor: :recycle: update index.js

--- index.js
+++ index.js
@@ -1,17 +1,11 @@
-//기본 양식
-
 const input = [];
+
+const strToNumArr = (str) => str.split(" ").map(Number);
 
 require("readline")
   .createInterface(process.stdin, process.stdout)
   .on("line", function (line) {
     input.push(line.trim());
-    /*
-    1
-    2 3
-    4 5 6
-    콘솔창을 통해 위와 같은 입력이 들어왔을 때,
-    input에 ['1', '2 3', '4 5 6']이 저장된다.
-    */
   })
-  .on("close", function () {});
+  .on("close", function () {
+  });
