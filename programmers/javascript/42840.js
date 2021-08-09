8d6a53d74251e22e60502da53a52252fc01e19b9
gywlsp <gywls517@gmail.com>
2021-08-09 17:19:25 +0900

    feat: :sparkles: add programmers 42840.js

---
+++ programmers/javascript/42840.js
@@ -0,0 +1,21 @@
+function solution(answers) {
+  const rules = [
+    [1, 2, 3, 4, 5],
+    [2, 1, 2, 3, 2, 4, 2, 5],
+    [3, 3, 1, 1, 2, 2, 4, 4, 5, 5],
+  ];
+  const rightCounts = [0, 0, 0];
+  answers.forEach((answer, aIndex) => {
+    rules.forEach((rule, person) => {
+      const _answer = rule[aIndex % rule.length];
+      if (answer === _answer) {
+        rightCounts[person] += 1;
+      }
+    });
+  });
+  const maxRightCount = Math.max(...rightCounts);
+
+  return rightCounts
+    .map((_, i) => i + 1)
+    .filter((n, i) => rightCounts[i] === maxRightCount);
+}
