eb21c82a84c7e2c289e30c2c4e6bb35ac030b992
gywlsp <gywls517@gmail.com>
2021-09-11 14:06:54 +0900

    feat: :sparkles: add goorm-epper 14-7.js

---
+++ goorm-epper/javascript/14-7.js
@@ -0,0 +1,38 @@
+const input = [],
+  words = [];
+
+function solution(r, c, zr, zc, words) {
+  let answer = [];
+
+  //코드를 작성해주세요.
+  words.forEach((word) => {
+    let str = "";
+    word.forEach((char) => {
+      for (let t = 0; t < zc; t++) {
+        str += char;
+      }
+    });
+    for (let t = 0; t < zr; t++) {
+      answer.push(str);
+    }
+  });
+
+  return answer;
+}
+
+require("readline")
+  .createInterface(process.stdin, process.stdout)
+  .on("line", function (line) {
+    input.push(line.trim());
+  })
+  .on("close", function () {
+    const [r, c, zr, zc] = input[0].split(" ");
+
+    for (let i = 1; i <= r; i++) {
+      words.push(input[i].split(""));
+    }
+
+    const answer = solution(r, c, zr, zc, words);
+    console.log(answer.join("\n"));
+    process.exit();
+  });
