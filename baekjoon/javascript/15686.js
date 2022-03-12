86832807492d0d2d8c8505c02f03eae6855dd753
gywlsp <gywls517@gmail.com>
2022-03-12 20:32:50 +0900

    add baekjoon 15686.js

---
+++ baekjoon/javascript/15686.js
@@ -0,0 +1,73 @@
+//https://www.acmicpc.net/problem/15686
+
+const input = [];
+
+require("readline")
+  .createInterface(process.stdin, process.stdout)
+  .on("line", function (line) {
+    input.push(line.trim());
+  })
+  .on("close", function () {
+    const strToNumArr = (str) => str.split(" ").map(Number);
+    const [N, M] = strToNumArr(input.shift());
+    const houses = [],
+      cHouses = [];
+    input.forEach((rowStr, r) => {
+      const row = strToNumArr(rowStr);
+      row.forEach((v, c) => {
+        if (v === 1) {
+          houses.push([r, c]);
+        }
+        if (v === 2) {
+          cHouses.push([r, c]);
+        }
+      });
+    });
+
+    let result = Number.MAX_SAFE_INTEGER;
+
+    const solve = ({
+      houses,
+      cHouses,
+      toSelect,
+      startIndex,
+      cIndexCombination,
+    }) => {
+      if (!toSelect) {
+        const cityCDist = getCityCDist({ houses, cHouses, cIndexCombination });
+        result = Math.min(result, cityCDist);
+      }
+      for (let i = startIndex; i < cHouses.length; i++) {
+        if (i > cHouses.length - toSelect) break;
+        solve({
+          houses,
+          cHouses,
+          toSelect: toSelect - 1,
+          startIndex: i + 1,
+          cIndexCombination: cIndexCombination.concat(i),
+        });
+      }
+    };
+
+    const getCityCDist = ({ houses, cHouses, cIndexCombination }) => {
+      const result = houses.reduce((acc, [row, col]) => {
+        let minD = Number.MAX_SAFE_INTEGER;
+        cIndexCombination.forEach((i) => {
+          const [r, c] = cHouses[i];
+          minD = Math.min(minD, Math.abs(row - r) + Math.abs(col - c));
+        });
+        return acc + minD;
+      }, 0);
+      return result;
+    };
+
+    solve({
+      houses,
+      cHouses,
+      toSelect: M,
+      startIndex: 0,
+      cIndexCombination: [],
+    });
+
+    console.log(result);
+  });
