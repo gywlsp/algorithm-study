3a7ed638920128ea578bece9853cd63a41983298
gywlsp <gywls517@gmail.com>
2022-03-21 22:00:15 +0900

    refactor: :recycle: refactor programmers 62756.js

--- programmers/javascript/67256.js
+++ programmers/javascript/67256.js
@@ -3,42 +3,48 @@ function solution(numbers, hand) {
     right = [3, 6, 9];
   let leftHand = 10,
     rightHand = 12;
+
+  const select = (number, hand) => {
+    if (hand === "left") {
+      leftHand = number;
+      return "L";
+    }
+    if (hand === "right") {
+      rightHand = number;
+      return "R";
+    }
+  };
+
+  const getDist = (a, b) => {
+    return (
+      Math.abs((a % 3) - (b % 3)) +
+      Math.abs(Math.floor(a / 3) - Math.floor(b / 3))
+    );
+  };
+
   const result = numbers.reduce((acc, curr) => {
     if (curr === 0) {
       curr = 11;
     }
     if (left.includes(curr)) {
-      leftHand = curr;
-      return acc + "L";
+      return acc + select(curr, "left");
     }
     if (right.includes(curr)) {
-      rightHand = curr;
-      return acc + "R";
+      return acc + select(curr, "right");
     }
-    const [lx, ly] = [
-      Math.abs(((curr - 1) % 3) - ((leftHand - 1) % 3)),
-      Math.abs(Math.floor((curr - 1) / 3) - Math.floor((leftHand - 1) / 3)),
-    ];
-    const [rx, ry] = [
-      Math.abs(((curr - 1) % 3) - ((rightHand - 1) % 3)),
-      Math.abs(Math.floor((curr - 1) / 3) - Math.floor((rightHand - 1) / 3)),
-    ];
-    const lDist = lx + ly,
-      rDist = rx + ry;
+
+    const lDist = getDist(curr - 1, leftHand - 1),
+      rDist = getDist(curr - 1, rightHand - 1);
     if (lDist < rDist) {
-      leftHand = curr;
-      return acc + "L";
+      return acc + select(curr, "left");
     }
     if (lDist > rDist) {
-      rightHand = curr;
-      return acc + "R";
+      return acc + select(curr, "right");
     }
     if (hand === "left") {
-      leftHand = curr;
-      return acc + "L";
+      return acc + select(curr, "left");
     }
-    rightHand = curr;
-    return acc + "R";
+    return acc + select(curr, "right");
   }, "");
   return result;
 }
